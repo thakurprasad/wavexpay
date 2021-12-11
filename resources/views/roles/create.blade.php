ca6718ebce8b6df4d619f7b00b2e57edb0914f22
manoj verma <manojvermaut@gmail.com>
2021-12-11 14:04:20 +0530

    roles: create form

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -19,18 +19,9 @@
         <div class="col s12 m12 l12">
           <div id="prefixes" class="card card card-default scrollspy">
             <div class="card-content">
-              @if(count($errors) > 0)
-                <div class="alert alert-danger">
-                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+              <div class="row">
 
-
+                @include('alerts.message')
 {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
 
               <h2 class="card-title">Create New Role</h2>
@@ -55,14 +46,11 @@
                     @endforeach
               </div>
 
-              <div class="row container">
-                <div class="input-field col s12">
-                  <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
-                    <i class="material-icons right">send</i>
-                  </button>
-                </div>
+               <div class="col s12 display-flex justify-content-end mt-3">
+                <button type="submit" class="btn cyan waves-effect waves-light right">
+                  Submit</button>
               </div>
-
+          </div>
 {!! Form::close() !!}
             </div>
         </div>
