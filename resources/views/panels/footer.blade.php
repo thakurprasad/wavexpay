1320ec257a47f43e5d251f1f94554fdb8d059c85
thakur prasad <thakurprasadlhs@gmail.com>
2021-12-11 17:30:12 +0530

    set page title and footer copyright section

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -3,11 +3,11 @@
   class="{{$configData['mainFooterClass']}} @if($configData['isFooterFixed']=== true){{'footer-fixed'}}@else {{'footer-static'}} @endif @if($configData['isFooterDark']=== true) {{'footer-dark'}} @elseif($configData['isFooterDark']=== false) {{'footer-light'}} @else {{$configData['mainFooterColor']}} @endif">
   <div class="footer-copyright">
     <div class="container">
-      <span>&copy; 2019 <a href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent"
-          target="_blank">PIXINVENT</a> All rights reserved.
+      <span>&copy; {{ date('Y') }} <a href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent"
+          target="_blank">{{ env('APP_NAME', 'WaveXpay') }}</a> All rights reserved.
       </span>
       <span class="right hide-on-small-only">
-        Design and Developed by <a href="https://pixinvent.com/">PIXINVENT</a>
+        Design and Developed by <a href="https://websoft-tech.com/">Websoft Technologies</a>
       </span>
     </div>
   </div>
