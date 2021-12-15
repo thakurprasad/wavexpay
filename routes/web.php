02667a6b37ad764d2bac0c41b23191f697351382
thakur prasad <thakurprasadlhs@gmail.com>
2021-12-15 13:26:46 +0530

    set GlobalController in web

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\GlobalController;
 
 /*
 |--------------------------------------------------------------------------
